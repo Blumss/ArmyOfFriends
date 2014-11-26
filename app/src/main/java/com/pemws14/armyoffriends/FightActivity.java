7e3fa266e6f14c0457188ff4a1106ed7e9fcdb59
LaraHa <artemis1987@gmx.net>
2014-11-26 16:15:50 +0100

    noch mehr drawer zeug

--- app/src/main/java/com/pemws14/armyoffriends/FightActivity.java
+++ app/src/main/java/com/pemws14/armyoffriends/FightActivity.java
@@ -1,19 +1,28 @@
 package com.pemws14.armyoffriends;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.pemws14.armyoffriends.drawer.BaseActivity;
 
 
 public class FightActivity extends BaseActivity {
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //<-- IN EVERY ACTIVITY WITH DRAWER
         super.set();
+        // catches Frame, where to insert actual ActivityView
+        ViewGroup parent = (ViewGroup) findViewById(R.id.content_frame);
+        //View of new activity
+        view = LayoutInflater.from(this).inflate(R.layout.activity_fight, parent, false);
+        parent.addView(view);
         //--> IN EVERY ACTIVITY WITH DRAWER
-    }
 
+    }
 
 }
