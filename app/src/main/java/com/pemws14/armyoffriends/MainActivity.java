5fb6d2e51f4a00418560cb32cb2fa76f95b3bcdb
LaraHa <artemis1987@gmx.net>
2015-01-22 19:43:19 +0100

    Standardprofilbild wird jetzt in DB gespeichert.

--- app/src/main/java/com/pemws14/armyoffriends/MainActivity.java
+++ app/src/main/java/com/pemws14/armyoffriends/MainActivity.java
@@ -480,7 +480,7 @@ public class MainActivity extends Activity implements GooglePlayServicesClient.C
         List<DbProfile> profiles = db.getAllProfiles();
         if(profiles.size()==0){
             Log.i("MainActivity.initDB", "No profiles found on device! Creating profile with ID " + parseUserID + ".");
-            dbProfile = new DbProfile(parseDb.getUserID(),parseDb.getCurrentUserName(),parseDb.getPlayerLevel(),parseDb.getEP(),parseDb.getArmyStrength(),parseDb.getMaxLevel());
+            dbProfile = new DbProfile(parseDb.getUserID(),parseDb.getCurrentUserName(),BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.profile_placeholder),parseDb.getPlayerLevel(),parseDb.getEP(),parseDb.getArmyStrength(),parseDb.getMaxLevel());
             db.createProfile(dbProfile);
         }else{
             for (DbProfile oneProfile: profiles) {
