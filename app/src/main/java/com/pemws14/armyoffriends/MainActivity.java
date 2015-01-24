9937b9de57c659dbf6d39506b39f16d9cdb9e2f9
LaraHa <artemis1987@gmx.net>
2015-01-24 20:12:27 +0100

    künstliche soldaten wieder rückgängig gemacht

--- app/src/main/java/com/pemws14/armyoffriends/MainActivity.java
+++ app/src/main/java/com/pemws14/armyoffriends/MainActivity.java
@@ -225,7 +225,7 @@ public class MainActivity extends Activity implements GooglePlayServicesClient.C
             }
         });
 
-        createSoldiers();
+        //createSoldiers();
     }
 
     /*
